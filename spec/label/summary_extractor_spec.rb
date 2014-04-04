31416e7cfba68b170bc0e81aeac20a2a18ae3527
Felipe Espinoza <fespinozacast@gmail.com>
2014-04-04 09:22:53 -0300

    split #extract_summary specs into their own contexts

--- spec/label/summary_extractor_spec.rb
+++ spec/label/summary_extractor_spec.rb
@@ -3,23 +3,37 @@ require "label/summary_extractor"
 describe Label::SummaryExtractor do
 
   describe "#extract_summary" do
-    it "should extract summary" do
-      # different scenarios regarding different types of string delimitations
-      content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = \"A series of generators an other rails 'defaults' for my projects\"\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
-      extractor = Label::SummaryExtractor.new(content)
-      expect(extractor.extract_summary).to eq "A series of generators an other rails 'defaults' for my projects"
+    # TODO: use contexts
+    context "for double quoted strings containing single quoted strings" do
+      it "should extract summary" do
+        content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = \"A series of generators an other rails 'defaults' for my projects\"\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
+        extractor = Label::SummaryExtractor.new(content)
+        expect(extractor.extract_summary).to eq "A series of generators an other rails 'defaults' for my projects"
+      end
+    end
 
-      content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = 'A series of generators an other rails \"defaults\" for my projects'\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
-      extractor = Label::SummaryExtractor.new(content)
-      expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my projects"
+    context "for single quoted strings containing double quoted strings" do
+      it "should extract summary" do
+        content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = 'A series of generators an other rails \"defaults\" for my projects'\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
+        extractor = Label::SummaryExtractor.new(content)
+        expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my projects"
+      end
+    end
 
-      content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = %q{A series of generators an other rails \"defaults\" for my 'projects'}\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
-      extractor = Label::SummaryExtractor.new(content)
-      expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my 'projects'"
+    context "for strings defined with %q{}" do
+      it "should extract summary" do
+        content = "s.email       = [\"fespinozacast@gmail.com\"]\n  s.homepage    = \"fespinoza.github.io\"\n  s.summary     = %q{A series of generators an other rails \"defaults\" for my 'projects'}\n  s.description = \"TODO.\"\n  s.license     = \"MIT\"\n\n"
+        extractor = Label::SummaryExtractor.new(content)
+        expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my 'projects'"
+      end
+    end
 
-      content = "g.email       = [\"fespinozacast@gmail.com\"]\n  g.homepage    = \"fespinoza.github.io\"\n  g.summary     = %q{A series of generators an other rails \"defaults\" for my 'projects'}\n  g.description = \"TODO.\"\n  g.license     = \"MIT\"\n\n"
-      extractor = Label::SummaryExtractor.new(content)
-      expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my 'projects'"
+    context "for different gem configuration variable names" do
+      it "should extract summary" do
+        content = "g.email       = [\"fespinozacast@gmail.com\"]\n  g.homepage    = \"fespinoza.github.io\"\n  g.summary     = %q{A series of generators an other rails \"defaults\" for my 'projects'}\n  g.description = \"TODO.\"\n  g.license     = \"MIT\"\n\n"
+        extractor = Label::SummaryExtractor.new(content)
+        expect(extractor.extract_summary).to eq "A series of generators an other rails \"defaults\" for my 'projects'"
+      end
     end
 
   end
